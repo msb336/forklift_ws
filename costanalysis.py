7516cf85dd0dddf6d185424e409b8b5c55b108fa
msb336 <msb336@drexel.edu>
2019-02-06 12:28:52 -0800

    added some stuff for error analysis

--- costanalysis.py
+++ costanalysis.py
@@ -11,12 +11,19 @@ def convertToPolar(cloud):
     r = np.sqrt(np.power(cloud[:,0],2) + np.power(cloud[:,1],2))
     theta = np.arctan(cloud[:,1]/cloud[:,0])
     return r, theta
-
-def getError(r,theta, distance, bounds):
+def isolate(r,theta,bounds):
     target_theta = theta[np.logical_and(theta>=bounds[0], theta<=bounds[1])]
     target_r = r[np.logical_and(theta>=bounds[0], theta<=bounds[1])]
     outer_theta = theta[np.logical_or(theta<bounds[0], theta > bounds[1])]
     outer_r = r[np.logical_or(theta<bounds[0], theta > bounds[1])]
+
+    return [target_theta, target_r], [outer_theta, outer_r]
+def getError(r,theta, distance, bounds):
+    target,outer = isolate(r,theta,bounds)
+    target_theta = target[0]
+    target_r = target[1]
+    outer_theta = outer[0]
+    outer_r = outer[1]
 
     in_error = np.linalg.norm(target_r-distance/np.cos(target_theta))
     out_error = np.linalg.norm(outer_r - distance/np.cos(outer_theta))
@@ -59,9 +66,16 @@ def getCost(point_cloud):
     cost = np.power((e1[1] - e2[1]-0.808),2)
 
     return cost
+def getLidarCount(client, iterations):
+    pointcloud = []
+    for i in range(iterations):
+        pointcloud += client.getLidarData().point_cloud
+    return pointcloud
 def getPolar(c):
-    l = c.getLidarData()
-    shaped_cloud = np.asarray(l.point_cloud).reshape(-1,3)[:,0:2]
+    # l = getLidarCount(c, 5)
+    # print(len(l))
+    l = c.getLidarData().point_cloud
+    shaped_cloud = np.asarray(l).reshape(-1,3)[:,0:2]
     r, theta = convertToPolar(shaped_cloud)
     return r, theta
 
@@ -115,51 +129,60 @@ def updatePlot(plot, fig, x,y):
     plot.clear()
     plot.plot(x,y, 'ro')
     fig.canvas.draw()
-# client = airsim.CarClient()
-# client.confirmConnection()
 
-
-# vehicle = client.simGetVehiclePose().position
-# client.simSetVehiclePose(Pose(Vector3r(0,0,1), Quaternionr()), True)
-# center = Vector3r(-4,0,1)
-offset_range = [0, 0.2]
+def avgError(c, iterations,dist,bound):
+    avgin=0
+    avgout=0
+    for i in range(iterations):
+        r,theta = getPolar(c)
+        iner,outer = getError(r,theta,dist,bound)
+        avgin+=iner
+        avgout+=outer
+    avgin=avgin/iterations
+    avgout=avgout/iterations
+    return avgin,avgout
 
 client, center = setup()
 sleep(0.2)
-# f = open('costanalysis.csv', 'w')
-# f.write('dispx,dispy,quatz,normalized-error,tolerance,cost\n')
+
+f = open('costanalysis-junk.csv', 'w')
+f.write('displacement x,displacement y,rotation,normalized error,inlier cost, outlier cost\n')
 
 tolerance = 5
 distance,bounds = getClosestPoint(client)
 r,theta = getPolar(client)
+target,outer=isolate(r,theta,bounds)
 print(distance, bounds)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-plot_data, = ax.plot(theta,r, 'ro')
+ax.plot(target[0],target[1], 'bo',label='inliers')
+ax.plot(outer[0],outer[1],'ro',label='outliers')
 ax.axis([-np.pi, np.pi, 0, 20])
 fig.show()
 inerr, outerr = getError(r,theta,distance,bounds)
-print("inbounds:", inerr, "outbounds", outerr)
 input()
 
-for i in range(101):
-
+max_y = 1
+iters = 100
+offset_range = [0, max_y]
+for i in range(iters+1):
+    # y = max_y*(i*2/iters - 1)
+    # rotation = 2*np.sin(2*np.pi*i/iters)
     random_pose, displacement, rotation = setRandomPose(center, offset_range)
+
     rotation_error = min(2-abs(rotation), abs(rotation))
+    normalized_error = np.sqrt(np.power(np.linalg.norm(displacement),2) + np.power(rotation_error,2))
+
     client.simSetObjectPose("pallet", random_pose)
-    sleep(0.2)
-    r,theta = getPolar(client)
-    inerr, outerr = getError(r,theta,distance,bounds)
-    print("inbounds:", inerr, "outbounds", outerr)
-    updatePlot(ax,fig,theta,r)
-    
-    input()
-    # pcdata = np.array((10,10,10))
-    # for i in range(5):
-    #     lidar = client.getLidarData()
-    #     pcdata = np.append(pcdata, np.asarray(lidar.point_cloud))
-    #     sleep(0.2)
+    sleep(0.1)
+    inerr,outerr=avgError(client,10,distance,bounds)
+    # r,theta = getPolar(client)
+    # inerr, outerr = getError(r,theta,distance,bounds)
+
+    f.write("{},{},{},{},{},{}\n".format(displacement[0], displacement[1], rotation_error, normalized_error, inerr, outerr))
+
+
     # if len(pcdata) > 3:
     #     cost = getCost(pcdata)
     #     save_data = np.concatenate((displacement.reshape(1,-1), 
@@ -170,4 +193,4 @@ for i in range(101):
     #         f.write("{},".format(save_data[i]))
     #     f.write("\n")
 
-# f.close()
+f.close()
