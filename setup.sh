7ac4b4580a6823f17b7ba1584c71c77797675b46
msb336 <msb336@drexel.edu>
2019-02-27 07:39:59 -0800

    updated to work with wsl

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 mkdir src; cd src
 git clone https://github.com/msb336/airsim_ros_pkgs.git
-git clone https://github.com/msb336/navigation.git -b custom-sub
-git clone https://github.com/ros-planning/navigation_msgs.git
-git clone https://github.com/ros/geometry2.git -b kinetic-devel
-git clone https://github.com/rst-tu-dortmund/teb_local_planner.git -b kinetic-devel
 git clone https://github.com/ros-drivers/ackermann_msgs.git
+
+sudo apt-get install ros-melodic-navigation
+sudo apt-get install ros-melodic-teb-local-planner
+
 cd ..
 catkin init
-catkin config --extend /opt/ros/kinetic
-catkin conif --merge-devel
-catkin config --cmake-args -DCMAKE_CXX_FLAGS=-std=c++11
 catkin build airsim_ros_pkg
-catkin build costmap_2d
-catkin build move_base
+catkin build ackerman_msgs
+
+sudo chmod +x /opt/ros/melodic/share/teb_local_planner/scripts/cmd_vel_to_ackermann_drive.py
