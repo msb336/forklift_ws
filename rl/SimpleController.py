b03473c92b197a18b1f683c15f79a164ab03a3f3
msb336 <msb336@drexel.edu>
2019-03-07 15:04:32 -0800

    modded to try and get transform buffer behaving

--- rl/SimpleController.py
+++ rl/SimpleController.py
@@ -16,8 +16,8 @@ class SimpleController(Reward):
     pid_control = PID(5,0.02,0.01,2)
     def calculateMotorControl(self, goal):
         des_angle = np.arctan(goal[1]/goal[0]) - np.pi/2
-        if goal[0] < 0:
-            des_angle -= np.sign(des_angle)*np.pi
+        #if goal[0] < 0:
+        #    des_angle -= np.sign(des_angle)*np.pi
         input = clip(self.pid_control.update(des_angle, 0, time.clock())[0])
 
 
--- src/neural_network_planner/scripts/neural_network_controller.py
+++ src/neural_network_planner/scripts/neural_network_controller.py
@@ -44,7 +44,7 @@ class NeuralNetworkController:
         self.goal_pose_pub = rospy.Publisher('/ml/goal_pose', PoseStamped, queue_size=10)
         self.pallet_sub = rospy.Subscriber('/airsim/pallet_pose', PoseStamped, self.pallet_cb)
         
-        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_buffer = tf2_ros.Buffer(rospy.Duration(1.0))
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
     def update(self):
         if self.control_implemented == True:
@@ -55,14 +55,15 @@ class NeuralNetworkController:
     def control(self):
         local_goal, global_goal, yaw = self.planner.update(self.sim_pose)
         global_msg = self.setPointMsg(global_goal)
-        local_goal_msg = self.tf_buffer.transform(global_msg, '/base_link')
-        local_goal[0] = local_goal_msg.pose.position.x
-        local_goal[1] = local_goal_msg.pose.position.y
+        local_goal_msg = self.tf_buffer.transform(global_msg, 'base_link')
+        local_goal[0] = local_goal_msg.point.x
+        local_goal[1] = local_goal_msg.point.y
 
         steering_angle, goal_angle = self.controller.calculateMotorControl(local_goal)
+        print(goal_angle)
         self.publishPoseMsg(goal_angle)
         
-        local_waypoint_msg = self.setPointMsg(local_goal, "/base_link")
+        local_waypoint_msg = self.setPointMsg(local_goal, "base_link")
         self.local_waypoint_pub.publish(local_goal_msg)
         speed = -0.65
         return speed, steering_angle
@@ -71,14 +72,14 @@ class NeuralNetworkController:
     def setVehicleCommandMessage(self, speed, steering_angle):
         msg = AckermannDriveStamped()
         msg.header.stamp = rospy.get_rostime()
-        msg.header.frame_id = '/world'
+        msg.header.frame_id = 'world'
         msg.drive.steering_angle = steering_angle
         msg.drive.speed = speed
         return msg
 
-    def setPointMsg(self, waypoint, frame='/world'):
+    def setPointMsg(self, waypoint, frame='world'):
         msg = PointStamped()
-        msg.header.stamp = rospy.get_rostime()
+        msg.header.stamp = rospy.get_rostime() #self.transform_time
         msg.header.frame_id = frame
         msg.point.x = waypoint[0]
         msg.point.y = waypoint[1]
@@ -87,10 +88,10 @@ class NeuralNetworkController:
     def publishPoseMsg(self, goal_angle):
         msg = PoseStamped()
         msg.header.stamp = rospy.get_rostime()
-        msg.header.frame_id = '/world'
-        msg.pose.position.x = self.sim_pose.position.x_val
-        msg.pose.position.y = self.sim_pose.position.y_val
-        msg.pose.position.z = self.sim_pose.position.z_val
+        msg.header.frame_id = 'base_link'
+        msg.pose.position.x = 0 
+        msg.pose.position.y = 0
+        msg.pose.position.z = 0
 
         msg.pose.orientation.x = 0
         msg.pose.orientation.y = 0
@@ -109,6 +110,7 @@ class NeuralNetworkController:
         orientation.y_val = sim_pose_msg.pose.orientation.y
         orientation.z_val = sim_pose_msg.pose.orientation.z
         self.sim_pose = Pose(pos, orientation)
+        self.transform_time = sim_pose_msg.header.stamp
     def pallet_cb(self, pallet_pose_msg):
         pos = Vector3r()
         orientation = Quaternionr()
