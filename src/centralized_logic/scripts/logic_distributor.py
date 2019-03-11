e5bd6e49238567e7d330a657f0c4a470e4a1519f
Matthew Brown <v-mattbr@microsoft.com>
2019-03-11 11:58:49 -0700

    added functions to do pallet goal transforms

--- src/centralized_logic/scripts/logic_distributor.py
+++ src/centralized_logic/scripts/logic_distributor.py
@@ -17,6 +17,96 @@ from enum import Enum
 import sys
 
 
+
+
+
+# epsilon for testing whether a number is close to zero
+_EPS = np.finfo(float).eps * 4.0
+
+# axis sequences for Euler angles
+_NEXT_AXIS = [1, 2, 0, 1]
+
+# TODO(lucasw) if sxyz works then eliminate the other possibilities
+# map axes strings to/from tuples of inner axis, parity, repetition, frame
+_AXES2TUPLE = {
+    'sxyz': (0, 0, 0, 0), 'sxyx': (0, 0, 1, 0), 'sxzy': (0, 1, 0, 0),
+    'sxzx': (0, 1, 1, 0), 'syzx': (1, 0, 0, 0), 'syzy': (1, 0, 1, 0),
+    'syxz': (1, 1, 0, 0), 'syxy': (1, 1, 1, 0), 'szxy': (2, 0, 0, 0),
+    'szxz': (2, 0, 1, 0), 'szyx': (2, 1, 0, 0), 'szyz': (2, 1, 1, 0),
+    'rzyx': (0, 0, 0, 1), 'rxyx': (0, 0, 1, 1), 'ryzx': (0, 1, 0, 1),
+    'rxzx': (0, 1, 1, 1), 'rxzy': (1, 0, 0, 1), 'ryzy': (1, 0, 1, 1),
+    'rzxy': (1, 1, 0, 1), 'ryxy': (1, 1, 1, 1), 'ryxz': (2, 0, 0, 1),
+    'rzxz': (2, 0, 1, 1), 'rxyz': (2, 1, 0, 1), 'rzyz': (2, 1, 1, 1)}
+
+def _euler_from_matrix(matrix, axes='sxyz'):
+    """temporaray import from https://github.com/matthew-brett/transforms3d/blob/master/transforms3d/_gohlketransforms.py for internal use only"""
+    firstaxis, parity, repetition, frame = _AXES2TUPLE[axes.lower()]
+
+
+    i = firstaxis
+    j = _NEXT_AXIS[i+parity]
+    k = _NEXT_AXIS[i-parity+1]
+
+    M = np.array(matrix, dtype=np.float64, copy=False)[:3, :3]
+    if repetition:
+        sy = math.sqrt(M[i, j]*M[i, j] + M[i, k]*M[i, k])
+        if sy > _EPS:
+            ax = math.atan2( M[i, j],  M[i, k])
+            ay = math.atan2( sy,       M[i, i])
+            az = math.atan2( M[j, i], -M[k, i])
+        else:
+            ax = math.atan2(-M[j, k],  M[j, j])
+            ay = math.atan2( sy,       M[i, i])
+            az = 0.0
+    else:
+        cy = math.sqrt(M[i, i]*M[i, i] + M[j, i]*M[j, i])
+        if cy > _EPS:
+            ax = math.atan2( M[k, j],  M[k, k])
+            ay = math.atan2(-M[k, i],  cy)
+            az = math.atan2( M[j, i],  M[i, i])
+        else:
+            ax = math.atan2(-M[j, k],  M[j, j])
+            ay = math.atan2(-M[k, i],  cy)
+            az = 0.0
+
+    if parity:
+        ax, ay, az = -ax, -ay, -az
+    if frame:
+        ax, az = az, ax
+    return ax, ay, az
+
+def _quaternion_matrix(quaternion):
+    """temporaray import from https://github.com/matthew-brett/transforms3d/blob/master/transforms3d/_gohlketransforms.py for internal use only"""
+    q = np.array(quaternion, dtype=np.float64, copy=True)
+    n = np.dot(q, q)
+    if n < _EPS:
+        return np.identity(4)
+    q *= math.sqrt(2.0 / n)
+    q = np.outer(q, q)
+    return np.array([
+        [1.0-q[2, 2]-q[3, 3],     q[1, 2]-q[3, 0],     q[1, 3]+q[2, 0], 0.0],
+        [    q[1, 2]+q[3, 0], 1.0-q[1, 1]-q[3, 3],     q[2, 3]-q[1, 0], 0.0],
+        [    q[1, 3]-q[2, 0],     q[2, 3]+q[1, 0], 1.0-q[1, 1]-q[2, 2], 0.0],
+        [                0.0,                 0.0,                 0.0, 1.0]])
+
+def _euler_from_quaternion(quaternion, axes='sxyz'):
+    """temporaray import from https://github.com/matthew-brett/transforms3d/blob/master/transforms3d/_gohlketransforms.py for internal use only"""
+    return _euler_from_matrix(_quaternion_matrix(quaternion), axes)
+def convertToEuler(quaternion):
+    return _euler_from_quaternion(quaternion, 'sxyz')[2]
+
+
+
+
+def linearExtension(x,y, q, distance):
+    rotation_angle = convertToEuler([q[0], 
+                                     q[1], 
+                                     q[2], 
+                                     q[3]])
+    #print(x,y, distance, rotation_angle)
+    return x + distance*np.cos(rotation_angle), y + distance*np.sin(rotation_angle)
+
+
 ######## FORKLIFT OPERATOR AND TASK ENUMS #####################
 class TASK(Enum):
     PICKUP = 1
@@ -131,7 +221,30 @@ class LogicDistributor:
 ######## ROS MSG CALLBACKS ##################
     # User goal setting subscriber callback
     def pickup_cb(self, package_location_msg):
-        self.package_pickup_msg = package_location_msg
+        world_package_msg = self.tf_buffer.transform(package_location_msg, "world")
+        self.package_pickup_msg = world_package_msg
+        x = world_pose_msg.pose.position.x
+        y = world_pose_msg.pose.position.y
+        z = world_pose_msg.pose.position.z
+        qw = world_pose_msg.pose.orientation.w
+        qz = world_pose_msg.pose.orientation.z
+        qx = world_pose_msg.pose.orientation.x
+        qy = world_pose_msg.pose.orientation.y
+
+        goal_x, goal_y = linearExtension(x,y,[qw,qx,qy,qz], 4)
+        goal_theta = world_pose_msg.pose.orientation.z
+        
+        self.airsim_goal_msg = PoseStamped()
+        self.airsim_goal_msg.pose.position.x = goal_x
+        self.airsim_goal_msg.pose.position.y = goal_y
+        self.airsim_goal_msg.pose.position.z = 0
+        self.airsim_goal_msg.pose.orientation.w = world_pose_msg.pose.orientation.w
+        self.airsim_goal_msg.pose.orientation.x = world_pose_msg.pose.orientation.x
+        self.airsim_goal_msg.pose.orientation.y = world_pose_msg.pose.orientation.y
+        self.airsim_goal_msg.pose.orientation.z = world_pose_msg.pose.orientation.z
+        self.airsim_goal_msg.header.seq = 1
+        self.airsim_goal_msg.header.frame_id = "world"
+
         self.control_logic = TASK.PICKUP
 
     # path status subscriber callback
@@ -169,6 +282,7 @@ class LogicDistributor:
         if self.operation_status is not STATUS.TRAVERSING:
             print("heading to pickup location")
             self.pallet_spawn_pub.publish(self.package_pickup_msg)
+            self.goal_pub.publish(self.airsim_goal_msg)
             self.controller = "ackermann"
             self.pickup_requested = True
             self.operation_status = STATUS.TRAVERSING
